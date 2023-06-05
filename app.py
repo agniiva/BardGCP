7511f1d88abb13b2f313a445591d225a16721403
Agniva Mahata <73607864+agniiva@users.noreply.github.com>
2023-06-05 14:51:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, request, jsonify
-from bardapi import Bard
 import os
+import bardapi
 
+# set your __Secure-1PSID value to key
+token = os.getenv('BARD_API_KEY', 'WQjHX5HN4ulBXXLzP-EcxKa9geXfIM3q1fxnF0nyrH3dItOH9D7vEIsCexfBZtK8mWApcg.')
 app = Flask(__name__)
-os.environ['_BARD_API_KEY'] = "WQjHX5HN4ulBXXLzP-EcxKa9geXfIM3q1fxnF0nyrH3dItOH9D7vEIsCexfBZtK8mWApcg."
 
 # Add the custom domain configuration
 app.config['SERVER_NAME'] = 'your-custom-domain.com'
 
 @app.route('/api', methods=['POST'])
 def get_bard_content():
+    # set your input text from request
     input_text = request.form.get('input')
-    content = Bard().get_answer(input_text)['content']
-    return jsonify({"content": content})
+    response = bardapi.core.Bard(token).get_answer(input_text)
+    return jsonify(response)
 
 if __name__ == '__main__':
     # Modify the app.run() line to listen on all available IPs
